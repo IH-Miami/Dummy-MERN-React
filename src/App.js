0c24bb5778ac41d0ab494038e02fb3bb4aa18b8c
IH-miami <mike.troianello@ironhack.com>
2022-07-27 18:48:25 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import { Routes, Route, useParams, useNavigate } from "react-router-dom";
+import AllPosts from "./components/AllPosts";
+import CreatePost from "./components/CreatePost";
+import Login from "./components/Login";
+import MyProfile from "./components/MyProfile";
+import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
+import ViewProfile from "./components/ViewProfile";
+import NotFound from "./components/NotFound";
+
+function App() {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [status, setStatus] = React.useState("");
+
+  const params = useParams();
+  const navigate = useNavigate();
+
+  let token = localStorage.getItem("token");
+
+  return (
+    <div className="App">
+      <Navbar token={token} />
+      <Routes>
+        <Route path="/" element={<AllPosts />} />
+        <Route path="/all-posts" element={<AllPosts />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/navbar" element={<Navbar />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/view-profile/:userId" element={<ViewProfile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = (props) => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/");
+  };
+
+  let name = localStorage.getItem("username");
+
+  return props.token ? (
+    <div>
+      <p>Welcome back, {name}!</p>
+      <Link to="/all-posts">All Posts</Link>
+      <Link to="/create-post">Create Post</Link>
+      <Link to="/my-profile">Profile</Link>
+      <button onClick={logout}>Log out</button>
+    </div>
+  ) : (
+    <div>
+      <Link to="/all-posts">All Posts</Link>
+      <Link to="/signup">Sign up</Link>
+      <Link to="/login">Log in</Link>
+    </div>
+  );
+};
+
+export default Navbar;
